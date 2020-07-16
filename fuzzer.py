6ec29ef5cdcb3309c5e312631ad4a591f8503c53
Frank Nguyen <fnguyen2000@hotmail.com>
2020-07-16 14:35:52 +1000

    Move input check functions to a separate file

--- fuzzer.py
+++ fuzzer.py
@@ -1,75 +1,17 @@
 #!/usr/bin/env python3
-from pwn import *
+from pwn import *   # pylint: disable=W0614
 import sys
-import csv
-import json
 import subprocess
 import os
-'''
-# NOTE: This passes for non-CSV files...
-# Given the sample input file, determines if it has the same format as a CSV file.
-def checkCSV(sampleInput):
-    try:
-        # The Sniffer function reads through the file and checks to see if it is
-        # a valid csv format.
-        csv.Sniffer().sniff(sampleInput.read())
 
-    # If an exception is raised, then this means it was not a valid csv format.
-    except:
-        print("Not a CSV file...")
-        return False
+from type_checker import checkCSV, checkJSON
 
-    return True
-'''
-
-# NOTE: This uses the assumption that a CSV file has equal number of commas on each
-# line, having more than 1 line and at least 1 comma.
-# Given the sample input file, determines if it has the same format as a CSV file.
-def checkCSV(sampleInput):
-
-    # First we read the file and split each line.
-    lines = sampleInput.readlines()
-
-    # We then count the number of times a comma appears on the first line.
-    commas = lines[0].count(",")
-
-    # We then check whether there is a comma on this line, as well as whether there
-    # is only one line in the file.
-    if commas == 0 or len(lines) == 1:
-        print("Not a CSV file...")
-        return False
-
-    # If it passes the initial check, then we can read through each line of the file
-    # and check the number of commas is equal for each line.
-    for line in lines:
-        if line.count(",") != commas:
-            print("Not a CSV file...")
-            return False
-
-    return True
-
-# Given the sample input file, determines if it has the same format as a JSON file.
-def checkJSON(sampleInput):
-    try:
-        # First we need to read the file and store its contents in a variable.
-        fileContents = sampleInput.read().strip()
-
-        # Then, the loads function attempts to decode the file's contents as
-        # json format.
-        json.loads(fileContents)
-
-    # If an exception is raised, then it is not valid json format.
-    except:
-        print("Not a JSON file...")
-        return False
-
-    return True
 
 # this will fuzz the binary with /dev/null for 200 times
 # if error found, will return True
 # all error messages (when process return value != 0) will be stored in ERRORDETAILS
 def urandomFuzzer(pathToBinary):
-    for i in range(0,200):
+    for _ in range(0,200):
         command = "cat /dev/urandom | " + pathToBinary
         retval = subprocess.call(command,shell=True,stdout=open(os.devnull, 'wb'),stderr=open(ERRORDETAILS,'a+'))
         if retval != 0:
---
+++ type_checker.py
@@ -0,0 +1,62 @@
+import csv
+import json
+
+'''
+# NOTE: This passes for non-CSV files...
+# Given the sample input file, determines if it has the same format as a CSV file.
+def checkCSV(sampleInput):
+    try:
+        # The Sniffer function reads through the file and checks to see if it is
+        # a valid csv format.
+        csv.Sniffer().sniff(sampleInput.read())
+
+    # If an exception is raised, then this means it was not a valid csv format.
+    except:
+        print("Not a CSV file...")
+        return False
+
+    return True
+'''
+
+# NOTE: This uses the assumption that a CSV file has equal number of commas on each
+# line, having more than 1 line and at least 1 comma.
+# Given the sample input file, determines if it has the same format as a CSV file.
+def checkCSV(sampleInput):
+
+    # First we read the file and split each line.
+    lines = sampleInput.readlines()
+
+    # We then count the number of times a comma appears on the first line.
+    commas = lines[0].count(",")
+
+    # We then check whether there is a comma on this line, as well as whether there
+    # is only one line in the file.
+    if commas == 0 or len(lines) == 1:
+        print("Not a CSV file...")
+        return False
+
+    # If it passes the initial check, then we can read through each line of the file
+    # and check the number of commas is equal for each line.
+    for line in lines:
+        if line.count(",") != commas:
+            print("Not a CSV file...")
+            return False
+
+    return True
+
+# Given the sample input file, determines if it has the same format as a JSON file.
+def checkJSON(sampleInput):
+    try:
+        # First we need to read the file and store its contents in a variable.
+        fileContents = sampleInput.read().strip()
+
+        # Then, the loads function attempts to decode the file's contents as
+        # json format.
+        json.loads(fileContents)
+
+    # If an exception is raised, then it is not valid json format.
+    except:
+        print("Not a JSON file...")
+        return False
+
+    return True
