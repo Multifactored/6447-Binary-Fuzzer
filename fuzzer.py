3780b97a366d31a10e1214583373e588952b9ad9
Frank Nguyen <fnguyen2000@hotmail.com>
2020-07-17 12:01:27 +1000

    Add __main__, clean `subprocess.call()`, catch unexpected exceptions

--- fuzzer.py
+++ fuzzer.py
@@ -15,7 +15,11 @@ context.log_level = 'error'
 def urandomFuzzer(pathToBinary):
     for _ in range(0,200):
         command = "cat /dev/urandom | " + pathToBinary
-        retval = subprocess.call(command,shell=True,stdout=open(os.devnull, 'wb'),stderr=open(ERRORDETAILS,'a+'))
+        retval = subprocess.call(command,
+                                 shell=True,
+                                 stdout=open(os.devnull, 'wb'),
+                                 stderr=open(ERRORDETAILS,'a+')
+                                )
         if retval != 0:
             return True
     return False
@@ -54,93 +58,96 @@ def runCSVFuzzer():
     checkBufferOverflowLines(sampleInput)
     # checkBufferOverflowColumns(sampleInput)
 
-# First check the correct number of arguments are given.
-if len(sys.argv) != 3:
-    # NEED TO MAKE SURE PROGRAM IS A PATH,NOT JUST THE BINARY NAME
-    # ./csv1 instead of csv1
 
-    sys.exit("Usage: ./fuzzer.py program sampleinput.txt")
+if __name__ == "__main__":
+    # First check the correct number of arguments are given.
+    if len(sys.argv) != 3:
+        # NEED TO MAKE SURE PROGRAM IS A PATH,NOT JUST THE BINARY NAME
+        # ./csv1 instead of csv1
 
-# We begin by opening the sample input file for reading, and extract the contents.
-try:
-    sampleInput = open(sys.argv[2], 'r')
-except:
-    sys.exit("Usage: ./fuzzer.py program sampleinput.txt")
+        sys.exit("Usage: ./fuzzer.py program sampleinput.txt")
 
-# file to store errors
-ERRORDETAILS = "errors.txt"
-INPUT_THAT_BREAKS = "bad.txt"
-# Next we want to determine the format of the sample input's contents.
+    # We begin by opening the sample input file for reading, and extract the contents.
+    try:
+        sampleInput = open(sys.argv[2], 'r')
+    except:
+        print("Error:", sys.exc_info()[0])
+        sys.exit("Usage: ./fuzzer.py program sampleinput.txt")
 
-# init flags
-isCSV = isJSON = isXML = False
+    # file to store errors
+    ERRORDETAILS = "errors.txt"
+    INPUT_THAT_BREAKS = "bad.txt"
+    # Next we want to determine the format of the sample input's contents.
 
-# We first check if it is in csv format.
-if checkCSV(sampleInput):
-    isCSV = True
-    print("Is a CSV file...")
+    # init flags
+    isCSV = isJSON = isXML = False
 
-# Once this is complete, we also need to reset the file pointer to the
-# beginning of the file for future reading/writing.
-sampleInput.seek(0)
+    # We first check if it is in csv format.
+    if checkCSV(sampleInput):
+        isCSV = True
+        print("Is a CSV file...")
 
-# Next we check if it is in json format.
-if checkJSON(sampleInput):
-    isJSON = True
-    print("Is a JSON file...")
+    # Once this is complete, we also need to reset the file pointer to the
+    # beginning of the file for future reading/writing.
+    sampleInput.seek(0)
 
-# TODO: CHECK XML, not required for midpoint
+    # Next we check if it is in json format.
+    if checkJSON(sampleInput):
+        isJSON = True
+        print("Is a JSON file...")
 
-# Once this is complete, we also need to reset the file pointer to the
-# beginning of the file for future reading/writing.
-sampleInput.seek(0)
+    # TODO: CHECK XML, not required for midpoint
 
-# Initially try fuzzing the following:
-# repeat 100 (cat /dev/urandom | program)
-# Then if this fails, move onto specialised fuzzes based on the sampleinput format
-# (could also try bit flipping)
+    # Once this is complete, we also need to reset the file pointer to the
+    # beginning of the file for future reading/writing.
+    sampleInput.seek(0)
 
-if urandomFuzzer(sys.argv[1]):
-    print("Found a crash from /dev/urandom input .... saving to " + ERRORDETAILS)
+    # Initially try fuzzing the following:
+    # repeat 100 (cat /dev/urandom | program)
+    # Then if this fails, move onto specialised fuzzes based on the sampleinput format
+    # (could also try bit flipping)
+
+    if urandomFuzzer(sys.argv[1]):
+        print("Found a crash from /dev/urandom input .... saving to " + ERRORDETAILS)
 
 
-# We would want to loop the following code, repeating for each new mutated input
-mutatedInput = sampleInput.read()
+    # We would want to loop the following code, repeating for each new mutated input
+    mutatedInput = sampleInput.read()
 
-# Run the program using pwntools, passing your mutated input as an argument.
+    # Run the program using pwntools, passing your mutated input as an argument.
 
-if isCSV:
-    runCSVFuzzer()
+    if isCSV:
+        runCSVFuzzer()
 
-p = process(sys.argv[1])
-p.send(mutatedInput)
-print("Current input is:\n{}".format(mutatedInput))
+    p = process(sys.argv[1])
+    p.send(mutatedInput)
+    print("Current input is:\n{}".format(mutatedInput))
 
-# If we got a crash, write the bad input to bad.txt
-# TODO: we need a harness to check whether the program crashed...
-# (note how when running csv1 with json1.txt, it states it stopped with exit code 0,
-# so I think we can use pwntools as the harness and check for the exit code)
-if False:
-    print("Found bad input.")
-    result = open(INPUT_THAT_BREAKS, "w+")
-    result.writelines([mutatedInput])
-    result.close()
-    exit()
+    # If we got a crash, write the bad input to bad.txt
+    # TODO: we need a harness to check whether the program crashed...
+    # (note how when running csv1 with json1.txt, it states it stopped with exit code 0,
+    # so I think we can use pwntools as the harness and check for the exit code)
+    if False:
+        print("Found bad input.")
+        result = open(INPUT_THAT_BREAKS, "w+")
+        result.writelines([mutatedInput])
+        result.close()
+        exit()
 
-p.close()
+    p.close()
 
-'''
-# Example byteflip function
-def byteflip(input):
-    b = bytearray(json, 'UTF-8')
+    '''
+    # Example byteflip function
+    def byteflip(input):
+        b = bytearray(json, 'UTF-8')
 
-    for i in range(0, len(b)):
-        if random.randint(0, 20) == 1:
-            b[i] ^= random.getrandbits(7)
+        for i in range(0, len(b)):
+            if random.randint(0, 20) == 1:
+                b[i] ^= random.getrandbits(7)
 
-    return b.decode('ascii)
+        return b.decode('ascii)
 
-# How to call the byte flip function
-for i in range(0, 100000):
-    yield byteflip(input)
-'''
+    # How to call the byte flip function
+    for i in range(0, 100000):
+        yield byteflip(input)
+    '''
