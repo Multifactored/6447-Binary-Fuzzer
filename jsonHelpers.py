ad2af5bd23f945bedd4fdd21e06a8bfb5e4f7720
Frank Nguyen <fnguyen2000@hotmail.com>
2020-07-18 13:57:14 +1000

    Initial attempt at randomising int value in json input

--- jsonHelpers.py
+++ jsonHelpers.py
@@ -20,32 +20,35 @@ def generateList():
 def jsonRandomTyped(jsonInput: dict, key_set: list):
     ''' Mutates values of each combination in the input to random values according to input value types '''
     mutatedJson = {}
+    output = []
 
     for subset in key_set:
         for key in subset:
             # find value type and generate random value according to that
-            if type(jsonInput[key]) == int:
+            val = jsonInput[key]
+            if type(val) == int:
                 # generate random int
                 val = generateInt()
 
-            elif type(jsonInput[key]) == str:
+            elif type(val) == str:
                 # generate random string
                 pass
 
-            elif type(jsonInput[key]) == list:
+            elif type(val) == list:
                 # generate random list
                 pass
 
             else:
-                sys.exit("Unexpected type:", type(jsonInput[key]), jsonInput[key])
+                sys.exit("Unexpected type:", type(val), val)
 
             mutatedJson[key] = val
         
-        yield mutatedJson
+        output.append(mutatedJson)
+    return
 
 
 if __name__ == "__main__":
-    x = []
+    key_set = []
 
     jsonInput = open(sys.argv[1], "r").read()
     jsonInput = json.loads(jsonInput)
@@ -54,6 +57,10 @@ if __name__ == "__main__":
 
     for i in range(1, len(choices) + 1):
         for combs in combinations(choices, i):
-            x.append(combs)
+            key_set.append(combs)
 
-    print(x)
+    print(key_set)
+
+    mutations = jsonRandomTyped(jsonInput, key_set)
+
+    print(mutations)
