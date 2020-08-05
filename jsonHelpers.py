fca30b766115ccf619728f00e947a553c31fae50
Frank Nguyen <fnguyen2000@hotmail.com>
2020-08-05 11:28:00 +1000

    Refactor the random but typed fuzzer, tweak the input size so that it increases exponentially in length for the  string generation

--- jsonHelpers.py
+++ jsonHelpers.py
@@ -5,43 +5,22 @@ import random
 from helper import *
 from itertools import combinations
 
-def generateInt():
-    return random.randint(-99999999,99999999)
-
-def generateStr():
-    choices = r"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-={}|[]\\:\";'<>?,./~`"
-    return ''.join(random.choice(choices) for i in range(random.randint(1,999)))
-
-def generateList():
-    return random.sample(range(0, 9999), random.randint(1,100))
 
 def jsonRandomTyped(jsonInput: dict, key_set: list):
     ''' Mutates values of each combination in the input to random values according to input value types '''
     output = []
 
-    for subset in key_set:
-        mutatedJson = jsonInput
-        for key in subset:
-            # find value type and generate random value according to that
-            val = jsonInput[key]
-            if type(val) == int:
-                # generate random int
-                val = generateInt()
+    for i in range(14):
+        for subset in key_set:
+            mutatedJson = jsonInput
+            for key in subset:
+                # find value type and generate random value according to that
+                val = jsonInput[key]
+                val = valTypeCheck(val, i)
 
-            elif type(val) == str:
-                # generate random string
-                val = generateStr()
+                mutatedJson[key] = val
 
-            elif type(val) == list:
-                # generate random list
-                val = generateList()
-
-            else:
-                sys.exit("Unexpected type:", type(val), val)
-
-            mutatedJson[key] = val
-
-        output.append(mutatedJson)
+            output.append(mutatedJson)
     return output
 
 def fuzzJSON(sampleInputFile, binary):
@@ -66,5 +45,4 @@ def fuzzJSON(sampleInputFile, binary):
 
 if __name__ == "__main__":
     # for testing
-    print(fuzzJSON(open("./binaries/json1.txt", "r"), "./binaries/json1"))
-    
+    fuzzJSON(open("./binaries/json1.txt", "r"), "./binaries/json1")
