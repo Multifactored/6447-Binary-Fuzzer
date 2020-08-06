6a2e428fb3b6c943ced1a4e8130b3d5962cdad04
mattimm <z5187551@student.unsw.edu.au>
2020-08-06 23:33:45 +1000

    xml1 done

--- xmlHelpers.py
+++ xmlHelpers.py
@@ -8,19 +8,47 @@ from itertools import combinations
 
 # TODO
 
+def recursive_find_all_tags(root,result):
+    for _ in root:
+        result.append(_)
+        recursive_find_all_tags(_,result)
+    return result
 
-def fuzzXML(sampleInputFile, binary, lock):
+def addForgedURLS(sampleInputFile, binary, lock):
     print("Fuzzing the XML formatted sample input...\n", end="")
-    print("Not fully implemented yet.\n", end="")
+    sampleInputFile.seek(0)
+    sampleInput = sampleInputFile.read()
 
-    # try inputting large sizes with all sorts of char injects
+    root = ET.fromstring(sampleInput)
+    
+    list_of_found_tags = recursive_find_all_tags(root,[])
+    all_tags_with_href = []
 
-    # try lots of new lines
+    for i in list_of_found_tags:
+        try:
+            if(i.attrib['href']):
+                all_tags_with_href.append(i)
+        except:
+            pass
+    # print(all_tags_with_href)
+    for i in all_tags_with_href:
+        i.set('href',"%s" * 100)
 
-    # try making any found links incredibly long with both regular and special chars
 
-    # try inputting a lot of random tags from a set list
+    # NEED TO ADD MORE VARIATIONS, THIS IS JUST FOR XML1
+    xmlstr = ET.tostring(root).decode()
+    if sendInputAndCheck(binary, xmlstr, lock):
+        return True, "Found vulnerability in XML!"
+    return False
 
-    # go ham with attributes
+# try having near infinite sub elements <div> <div> <div> <div>...</div></div></div></div>
 
-    return
+# try inputting large sizes with all sorts of char injects
+
+# try lots of new lines
+
+# try making any found links incredibly long with both regular and special chars
+
+# try inputting a lot of random tags from a set list
+
+# go ham with attributes
