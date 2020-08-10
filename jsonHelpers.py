53a09fd22e15f739f7570d0519da15affbb98b0d
Frank Nguyen <fnguyen2000@hotmail.com>
2020-08-10 13:11:15 +1000

    Create function to generate random string with higher chance of curly braces, quotes and colons to generate invalid JSON and use generateBadJson function from helpers.py

--- jsonHelpers.py
+++ jsonHelpers.py
@@ -7,6 +7,24 @@ from itertools import combinations
 import copy
 
 
+def brokenJson(jsonInput: dict, key_set: list, maxPower: int):
+    # similar to generateStr() except this has a higher chance of having the characters {}"
+    choices = r'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890::::::{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"{}"'
+    maxPower = 2 ** maxPower
+    output = "{" + "\"" + generateStr(0) + "\": "+ ''.join(random.choice(choices) for i in range(maxPower)) + "}"
+
+    return output
+
+
+def jsonInJson(jsonInput: dict, key_set: list):
+    output = {}
+    jsonStr = json.dumps(jsonInput)
+    for key in jsonInput.keys(): 
+        output[key] = jsonStr * 20
+
+    return output
+
+
 def jsonRandomTyped(jsonInput: dict, key_set: list):
     ''' Mutates values of each combination in the input to random values according to input value types '''
     output = []
@@ -41,6 +59,19 @@ def fuzzJSON(sampleInputFile, binary, lock):
         for combs in combinations(choices, i):
             key_set.append(combs)
 
+    for i in range(10):
+        for j in range(10):
+            if sendInputAndCheck(binary, brokenJson(jsonInput, key_set, j), lock):
+                return True, "Found vulnerability in JSON!"
+
+    for i in range(10):
+        badJson = generateBadJson(i).replace('\\"',"\"")
+        if sendInputAndCheck(binary, badJson, lock):
+            return True, "Found vulnerability in JSON!"
+
+    if sendInputAndCheck(binary, json.dumps(jsonInJson(jsonInput, key_set)), lock):
+        return True, "Found vulnerability in JSON!"
+
     mutations = jsonRandomTyped(jsonInput, key_set)
     for i in mutations:
         if sendInputAndCheck(binary, json.dumps(i), lock):
@@ -50,4 +81,5 @@ def fuzzJSON(sampleInputFile, binary, lock):
 
 if __name__ == "__main__":
     # for testing
-    fuzzJSON(open("./binaries/json1.txt", "r"), "./binaries/json1")
+    print(generateBadJson(10))
+    fuzzJSON(open("./binaries/json2.txt", "r"), "./binaries/json2", "")
